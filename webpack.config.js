f07f5c6a6312a1a98e91d4c3d7792ecd1f142c9a
AmerikVelasquez <amerik.velasquez22@gmail.com>
2021-07-28 20:27:38 -0700

    correct type

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-module.exprts = {
+module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'bundle.js',
